f72c8c0143eea38de8443c3af46812fb18600557
Hurian Yahya Tebe <fullcompetitive7@gmail.com>
2026-09-26 20:21:30 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -218,7 +218,6 @@ export default function App() {
           alignItems: 'center',
           justifyContent: 'center',
           padding: 'clamp(10px, 4vw, 24px)',
-          padding: 24,
         }}
       >
         {body}
